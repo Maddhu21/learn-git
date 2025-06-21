9c2024a2515100f384d509c3859b76b8fa540f6c
Muhammad Haziq Luqman Bin Alias <74994555+Maddhu21@users.noreply.github.com>
2025-06-22 07:24:04 +0800

    Resolve merge conflict

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,5 @@
 <div>
     <!-- Simplicity is an acquired taste. - Katharine Gerould -->
-    <span>This is a welcome message</span>
+    <span>Welcome to My Application</span>
+    <span>Login</span>
 </div>
